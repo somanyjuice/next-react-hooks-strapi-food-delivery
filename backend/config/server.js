96ce96a156588032774f61a9da920c77e031a451
somanyjuice <somanyjuice@naver.com>
2021-03-04 18:12:26 +0900

    1.0 Setup

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e3cafc9f59bdd65576991b06dd8e965d'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4edf8374-b816-4163-aa95-2ee39b757361'
+};
