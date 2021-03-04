a0caf51899f7c27d10f6842dd86257e7145e26bd
somanyjuice <somanyjuice@naver.com>
2021-03-04 21:50:00 +0900

    2.0 Restaurants

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,8 +2,9 @@ import React from "react";
 import App from "next/app";
 import Head from "next/head";
 import Layout from "../components/Layout";
+import withData from "../lib/apollo";
 
-export default class MyApp extends App {
+class MyApp extends App {
     render() {
       const { Component, pageProps } = this.props;
       return (
@@ -23,3 +24,5 @@ export default class MyApp extends App {
       );
     }
   }
+
+export default withData(MyApp);
